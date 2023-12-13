2229ede8936208b3e72279166d87d7f8e33b45d6
Renee Paczkowski <rpaczkowski@gmail.com>
2023-12-13 13:56:28 -0500

    Trying a simpler RegEx code

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,15 +1,18 @@
 import input from "./day1-array";
 
-const str = input;
+let str = input;
+let sum = 0;
 
-const firstNumber = str
-  .replace(/\d+\.\d+|\d+\b|\d+(?=\w)/g || [])
-  .map(function (v) {
-    return +v;
-  })(/\d+\.\d+|\d+\b|\d+(?=\w)/g || [])
-  .map(function (v) {
-    return +v;
-  })
-  .pop();
+let firstNumber = str.match(/\d+/g);
 
 console.log(firstNumber);
+
+if (matches) {
+  console.log(matches[0] + ", " + matches[1] + ", " + matches[2]);
+}
+
+// for (let i = 0; i < myNums.length; i++) {
+//   sum += myNums[i];
+// }
+
+// console.log(sum);
