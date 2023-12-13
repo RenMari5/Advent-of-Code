1fac0036f81c3d8df884ed7bfade61edacd8e01b
Renee Paczkowski <rpaczkowski@gmail.com>
2023-12-13 12:03:42 -0500

    Tried RegEx

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,1 +1,15 @@
 import input from "./day1-array";
+
+const str = input;
+
+const firstNumber = str
+  .replace(/\d+\.\d+|\d+\b|\d+(?=\w)/g || [])
+  .map(function (v) {
+    return +v;
+  })(/\d+\.\d+|\d+\b|\d+(?=\w)/g || [])
+  .map(function (v) {
+    return +v;
+  })
+  .pop();
+
+console.log(firstNumber);
