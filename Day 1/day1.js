16ad85a525a6ce2f4f39fd70ab144a9057140048
Renee Paczkowski <rpaczkowski@gmail.com>
2023-12-18 11:21:22 -0500

    Tried a different solution with an API

--- Day 1/day1.js
+++ Day 1/day1.js
@@ -1,20 +1,27 @@
-import input from "./day1-array";
+const response = await fetch("https://adventofcode.com/2023/day/1/input");
+const txt = await response.text();
+const inputs = txt.trim().split("\n");
 
-let str = input;
-let num = "0123456789";
+//Part 1
+function part1() {
+  let sum = 0;
 
-// let sum = 0;
+  //iterate over each line
+  inputs.forEach((input) => {
+    let digit1, digit2;
 
-let matches = str.match(/\d+/g).map(Number);
+    //check each char individually
+    for (var x of input) {
+      //if char is a number and digit1 doesn't yet exist (this will act as "first index of")
+      if (!isNaN(x) && !digit1) digit1 = x;
 
-if (matches) {
-  console.log(matches[0] + ", " + matches[1] + ", " + matches[2]);
+      //if char is a number (this will act as "last index of")
+      if (!isNaN(x)) digit2 = x;
+    }
+
+    //sum up results per input
+    sum += Number(digit1.concat(digit2));
+  });
+  return sum;
 }
-
-console.log(matches);
-
-// for (let i = 0; i < myNums.length; i++) {
-//   sum += myNums[i];
-// }
-
-// console.log(sum);
+console.log(part1());
